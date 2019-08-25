f8ea49f3d8e94b4e5dce20e8cd3fdc76c8a5b0cd
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-25 09:57:35 -0700

    init babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require ('webpack');
+const dotEnv = require ('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: "development",
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new dotEnv(),
+    // new webpack.DefinePlugin({
+    //   'clientID': JSON.stringify(process.env.CLIENT_ID)
+    // })
+  ]
+};
