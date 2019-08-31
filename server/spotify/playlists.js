a79579b8958f555a1ff1227b2f6b14c28dc618d7
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-30 17:37:37 -0700

    refac to use central spotify get calls

--- server/spotify/playlists.js
+++ server/spotify/playlists.js
@@ -1,21 +1,17 @@
-const axios = require ('axios');
-const genAuthHeader = require('./authorize').generateAccessAuthorizationStr;
+const {getFromSpotify} = require ('./spotifyRequest');
 
 module.exports = {
   getPlaylists (accessToken, offset = 0) {
-    return axios.get('https://api.spotify.com/v1/me/playlists', {
-      headers: {
-        Authorization: genAuthHeader(accessToken)
-      },
+    return getFromSpotify('https://api.spotify.com/v1/me/playlists', accessToken, {
       params: {
         limit: 50,
         offset
       }
     }).then (async res => {
-      if (res.data.items.length === res.data.limit) {
+      if (res.items.length === res.limit) {
         res.data.items.push(...await module.exports.getPlaylists(accessToken, res.data.limit))
       }
-      return res.data.items;
+      return res.items;
     })
   }
 }
--- server/spotify/userData.js
+++ server/spotify/userData.js
@@ -1,14 +1,33 @@
-const axios = require ('axios');
-const genAuthHeader = require('./authorize').generateAccessAuthorizationStr;
+const {getFromSpotify} = require ('./spotifyRequest');
+const {getPlaylists} = require ('./playlists');
+
+const getAllSongsDefaults = {
+  includePlaylists: true,
+  includeLikedSongs: true,
+  includedLikedAlbums: true,
+}
+
+const getUserData = function getUserData (accessToken) {
+  return getFromSpotify('https://api.spotify.com/v1/me', accessToken).then(res => {
+    return res;
+  }).catch (err =>  {
+    console.error(err)
+  })
+}
+
+const getAllSongsFromUser = async function getAllSongsFromUser (accessToken, options = getAllSongsDefaults) {
+  options = Object.create (getAllSongsDefaults, options);
+  const songs = new Set();
+  const requests = [];
+
+  requests[0] = options.includePlaylists ? getPlaylists(accessToken) : Promise.resolve([]);
+  requests[1] = options.includeLikedSongs ? getPlaylists(accessToken) : Promise.resolve([]);
+  requests[2] = options.includedLikedAlbums ? getPlaylists(accessToken) : Promise.resolve([]);
+
+  const [retrievedPlaylists, retireivedLikedSongs, retrievedLikedAlbums] = await Promise.all(requrests);
+}
+
 module.exports = {
-  getUserData (accessToken) {
-    return axios.get('https://api.spotify.com/v1/me', {
-      headers:  {Authorization: genAuthHeader(accessToken)}
-      // headers:  {Authorization: `Bearer ${accessToken}`}
-    }).then(res => {
-      return res.data;
-    }).catch (err =>  {
-      console.error(err)
-    })
-  }
+  getAllSongsFromUser,
+  getUserData,
 }
