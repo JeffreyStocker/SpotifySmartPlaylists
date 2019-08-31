f20ae2755d8d2276a273ca7b32007a63c10e3903
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-30 17:35:32 -0700

    added query string middleware and removed code

--- server/server.js
+++ server/server.js
@@ -2,18 +2,20 @@ const dotEnv = require('dotenv').config();
 
 const koa = require ('koa');
 const static = require ('koa-static')
-const Router = require ('koa-router');
-const path = require ('path')
 const bodyParser = require('koa-bodyparser');
+const qs = require('koa-qs');
 
 const authorize = require('./routes/authorize');
 const database = require ('./database/database');
+const test = require ('./routes/test');
 
 const app = new koa();
 const port = Number(process.env.PORT) || 8080;
 
+qs(app);
 app.use (bodyParser());
 app.use(static(__dirname + '/../dist'))
 app.use(authorize.routes(), authorize.allowedMethods());
+app.use(test.routes(), test.allowedMethods());
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
