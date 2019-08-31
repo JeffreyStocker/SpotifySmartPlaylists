0c217407ed43798105820fa06f786526ccf352c1
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-31 12:13:26 -0700

    feat: interate through tracks from spotify

--- server/spotify/tracks.js
+++ server/spotify/tracks.js
@@ -1,6 +1,7 @@
 const {getFromSpotify, getFromSpotifyWithLimits} = require ('./spotifyRequest');
 const _flatten = require('lodash/flatten')
 const {TRACKS_TRACKS_URL, REQUEST_LIMIT} = require ('../constants');
+const {createOptions} = require ('../utilities/utilityFunction');
 
 const requestTracks =  function getTracks (accessToken, ids = []) {
   return getFromSpotify(TRACKS_TRACKS_URL, accessToken, {
@@ -18,7 +19,15 @@ const getTracks = async function getTracks (accessToken, ids = []) {
   })
 }
 
+const eachTrack = function eachTrack (tracks, callback, options = {}) {
+  options = createOptions({ignoreLocal: false}, options);
+  tracks.forEach((track, index, tracks) => {
+    !ignoreLocal && !track.track.is_local && callback(track.track, index, tracks);
+  })
+}
+
 module.exports = {
   getTracks,
-  requestTracks
+  requestTracks,
+  eachTrack
 }
