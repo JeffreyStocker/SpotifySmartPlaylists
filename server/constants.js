49d0ee7d37c0f86613edd7fa19df2f2769cccdb6
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-30 17:33:24 -0700

    seperated out constants to own file

---
+++ server/constants.js
@@ -0,0 +1,23 @@
+module.exports = {
+  //spotify api
+    //library
+    SAVED_TRACKS_URL: 'https://api.spotify.com/v1/me/tracks',
+    SAVED_ALBUMS_URL: "https://api.spotify.com/v1/me/albums",
+
+    //user
+
+    //tracks
+    TRACKS_AUDIO_ANALYSIS_URL: '/v1/audio-analysis/{id}',
+    TRACKS_AUDIO_FEATURE_URL: '/v1/audio-features/{id}',
+    TRACKS_AUDIO_FEATURES_URL: '/v1/audio-features',
+    TRACKS_TRACKS_URL: '/v1/tracks',
+    TRACKS_TRACK_URL: '/v1/tracks/{id}',
+
+    //albums
+
+    //artists
+
+    //other
+    REQUEST_LIMIT: 50,
+
+}
