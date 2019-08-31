25225b69c50ccd87045221ff193d6853bf6a9ffa
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-30 22:07:20 -0700

    refac requestTracks to use getFromSpotify/wLimits

--- server/spotify/tracks.js
+++ server/spotify/tracks.js
@@ -1,8 +1,6 @@
-const {getFromSpotify} = require ('./spotifyRequest');
+const {getFromSpotify, getFromSpotifyWithLimits} = require ('./spotifyRequest');
 const _flatten = require('lodash/flatten')
-const {TRACKS_TRACKS_URL} = require ('../constants');
-
-const REQUEST_LIMIT = 50;
+const {TRACKS_TRACKS_URL, REQUEST_LIMIT} = require ('../constants');
 
 const requestTracks =  function getTracks (accessToken, ids = []) {
   return getFromSpotify(TRACKS_TRACKS_URL, accessToken, {
@@ -10,25 +8,14 @@ const requestTracks =  function getTracks (accessToken, ids = []) {
       ids: ids.join(',')
     }
   })
-  .then (res => {
-    return res.data;
-  });
 }
 
 const getTracks = async function getTracks (accessToken, ids = []) {
-  const tracks = [];
-  if (ids.length < REQUEST_LIMIT + 1) {
-    tracks.push(requestTracks(accessToken, ids));
-  } else {
-    for (let i = 0; i < ids.length; i += REQUEST_LIMIT) {
-      const diff = ids.length - i;
-      tracks.push(
-        requestTracks(accessToken, ids.slice(i, diff < REQUEST_LIMIT ? ids.length - i : ids.length - 1))
-      )
+  return getFromSpotifyWithLimits(TRACKS_TRACKS_URL, accessToken, {
+    params: {
+      id: ids.join(',')
     }
-  }
-  const retrievedTracks = await Promise.all(tracks);
-  return _flatten(retrievedTracks);
+  })
 }
 
 module.exports = {
