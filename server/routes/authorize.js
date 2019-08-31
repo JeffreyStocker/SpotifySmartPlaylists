b0962e6a647647499b33ac3c4cae258134983535
Jeffrey Stocker <kagesennin@gmail.com>
2019-08-30 17:36:22 -0700

    refac authorize

--- server/routes/authorize.js
+++ server/routes/authorize.js
@@ -3,21 +3,27 @@ const router = Router();
 const {getToken} = require ('../spotify/authorize');
 const {getUserData} = require ('./../spotify/userData');
 const {getPlaylists} = require ('./../spotify/playlists');
-const User = require ('../database/users');
+const {createOrUpdateUser} = require ('../database/users');
 
 router.post('/authorize', async function (ctx, next) {
   const code = ctx.request.body.code;
-  const token = await getToken(code);
-  const accessToken = token.access_token;
+  const tokenData = await getToken(code);
+  const {access_token, refresh_token} = tokenData;
 
-  const userData = await getUserData(accessToken);
-  const playlists = await getPlaylists(accessToken);
-  User.createUser({'test': 'yest'})
+  const [userData, playlists] = await Promise.all ([await getUserData(access_token), await getPlaylists(access_token)])
+
+  const results = await createOrUpdateUser({
+    id: userData.id,
+    name: userData.display_name,
+    accessToken: access_token,
+    refreshToken: refresh_token
+  })
 
   ctx.body = {
     userData,
     playlists
   };
+
   return ctx;
 })
 
