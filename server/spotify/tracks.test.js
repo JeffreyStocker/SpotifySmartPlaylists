d61c4195697f9d25ee50b70b508e23fcee986d2c
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-01 17:11:31 -0700

    refac tracks test for spotify calling

--- server/spotify/tracks.test.js
+++ server/spotify/tracks.test.js
@@ -1,45 +1,112 @@
 process.env.CLIENT_ID = 'CLIENT_ID';
 process.env.CLIENT_SECRET = 'CLIENT_SECRET';
 process.env.SPOTIFY_REDIRECT = "localhost"
-const tracks = require ('./tracks');
-const axios = require ('axios');
+const {
+  getTracks,
+  requestTracks,
+  eachTrack,
+  getAllAlbumsIdsFromTracks} = require ('./tracks');
+const spotifyRequest = require ('./spotifyRequest');
+const {TRACKS_TRACKS_URL} = require ('../constants')
 
-jest.mock('axios');
+jest.mock('./spotifyRequest');
 
+const {getFromSpotify, getFromSpotifyWithLimits} = spotifyRequest;
+const resetSpotify = function ( ) {
+  getFromSpotify. mockClear();
+  getFromSpotifyWithLimits.mockClear();
+}
 
-describe ('track api request to spotify', () => {
-  const resp = {test: 'yes'};
-  test('should call axios once for under 50 items', () => {
-    axios.get.mockResolvedValue(Promise.resolve(resp));
-    return tracks.getTracks('token', ['3243424', '312412312', '53435']).then(results => {
-      expect(axios.get.mock.calls.length).toEqual(1);
+describe('module tracks', () => {
+  describe ('function getTracks', () => {
+    afterEach(resetSpotify);
+    it ('should return a promise', () => {
+      const tracks = getTracks('sometoken', ['234234234', '54653563', '2626247', '4643652'])
+      expect(tracks).toBeInstanceOf(Promise);
+      tracks.then (()=> {})
+    })
+
+    it ('should have called getFromSpotify 1 time', () => {
+      const tracks = getTracks('sometoken', ['234234234', '54653563', '2626247', '4643652'])
+      return tracks.then (()=> {
+        return expect(getFromSpotifyWithLimits).toHaveBeenCalledTimes(1);
+      })
+    })
+
+    it ('should resolve into data recieved from getAllAlbumsIdsFromTracks', () => {
+      const testData = {
+        tracks: [
+          {name: 1},
+          {name: 2}
+        ]
+      }
+      getFromSpotifyWithLimits.mockReturnValue(Promise.resolve(testData));
+      const tracks = getTracks('someToken', ['3523532', '532532', '3463464', '35326326'])
+        return tracks.then (results => expect(results).toBe(testData))
+    })
+
+    it('should send the correct pararmeters to getAllAlbumsIdsFromTracks', () => {
+      const token = 'someToken'
+      const ids = ['3523532', '532532', '3463464', '35326326'];
+      const tracks = getTracks(token, ids)
+      return tracks.then (results => {
+        expect(getFromSpotifyWithLimits).toHaveBeenCalledWith(TRACKS_TRACKS_URL, token, expect.any(Object))
+        return expect(getFromSpotifyWithLimits.mock.calls[0][2]).toMatchObject({params: {ids: ids.join(',')}})
+      })
     })
   })
-  test('should call axios twice for above 50 but under 101 items', () => {
-    axios.get.mockClear();
-    return tracks.getTracks('token', new Array(75).fill(342323)).then(results => {
-      expect(axios.get.mock.calls.length).toEqual(2);
-    }).catch(err => {console.error (err)})
+
+  describe ('function requestTracks', () => {
+    afterEach(resetSpotify);
   })
 
-  test ('should call axios 3 times for above 100 but under 151', () => {
-    axios.get.mockClear();
-    tracks.getTracks('token', new Array(125).fill(342323)).then(results => {
-      return expect(axios.get.mock.calls.length).toEqual(3);
-    })
-  })
-  test('should call 3 times for 101 items', () => {
-    axios.get.mockClear();
-    tracks.getTracks('token', new Array(101).fill(342323)).then(results => {
-      return expect(axios.get.mock.calls.length).toEqual(3);
-    })
-  })
-  test('should call 3 times for 150 items', () => {
-    axios.get.mockClear();
-    tracks.getTracks('token', new Array(150).fill(342323)).then(results => {
-      return expect(axios.get.mock.calls.length).toEqual(3);
-    })
-  })
+  describe ('function eachTrack', () => {
+    afterEach(resetSpotify);
 
 
+  })
+
+  describe ('function getAllAlbumsIdsFromTracks', () => {
+    afterEach(resetSpotify);
+
+    const tracks = [
+      {
+        track: {
+          album: {
+            id: 'test1',
+          },
+          is_local: false
+        }
+      },
+      {
+        track: {
+          album: {
+            id: 'test2'
+          },
+          is_local: true
+        }
+      }
+    ];
+
+    it('should return ids of albums from list of tracks', () => {
+
+      return getAllAlbumsIdsFromTracks(tracks).then (ids => {
+        expect(ids).toBeInstanceOf(Set);
+        const keys = Array.from(ids.keys());
+        expect(keys).toContain('test1');
+        expect(keys).not.toContain('test2');
+        return;
+      })
+    })
+
+    it ('should return list of ids included local tracks', () => {
+      return getAllAlbumsIdsFromTracks(tracks, {ignoreLocal: false}).then (ids => {
+        expect(ids).toBeInstanceOf(Set);
+        const keys = Array.from(ids.keys());
+        expect(keys).toContain('test1');
+        expect(keys).toContain('test2');
+        return;
+      })
+    })
+  })
 })
