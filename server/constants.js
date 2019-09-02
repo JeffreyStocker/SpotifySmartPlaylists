f9813fcefa13d221bf86e91c725e33f7e27e3d3d
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-02 08:41:05 -0700

    bug: fixed incorrect getArtistURL

--- server/constants.js
+++ server/constants.js
@@ -7,37 +7,38 @@ module.exports = {
     //user
 
     //tracks
-    TRACKS_AUDIO_ANALYSIS_URL: '/v1/audio-analysis/{id}',
-    getAudioAnalysisURL: (id) => `/v1/audio-analysis/${id}`,
-    TRACKS_AUDIO_FEATURE_URL: '/v1/audio-features/{id}',
-    getAudioFeaturesURL: (id) => `/v1/audio-features/${id}`,
-    TRACKS_AUDIO_FEATURES_URL: '/v1/audio-features',
-    TRACKS_TRACKS_URL: '/v1/tracks',
-    TRACKS_TRACK_URL: '/v1/tracks/{id}',
-    getAudioTrackURL: (id) => `/v1/tracks/${id}`,
+    TRACKS_AUDIO_ANALYSIS_URL: 'https://api.spotify.com/v1/audio-analysis/{id}',
+    getAudioAnalysisURL: (id) => `https://api.spotify.com/v1/audio-analysis/${id}`,
+    TRACKS_AUDIO_FEATURE_URL: 'https://api.spotify.com/v1/audio-features/{id}',
+    getAudioFeaturesURL: (id) => `https://api.spotify.com/v1/audio-features/${id}`,
+    TRACKS_AUDIO_FEATURES_URL: 'https://api.spotify.com/v1/audio-features',
+    TRACKS_TRACKS_URL: 'https://api.spotify.com/v1/tracks',
+    TRACKS_TRACK_URL: 'https://api.spotify.com/v1/tracks/{id}',
+    getAudioTrackURL: (id) => `https://api.spotify.com/v1/tracks/${id}`,
 
 
     //albums
     ALBUMS_GET_ALBUMS_MAX_REQUESTS: 20,
-    ALBUMS_GET_ALBUM_URL: '/v1/albums/{id}',
-    getAlbumURL: (id) => `/v1/albums/${id}`,
-    ALBUMS_GET_ALBUMS_URL: '/v1/albums/',
+    ALBUMS_GET_ALBUM_URL: 'https://api.spotify.com/v1/albums/{id}',
+    getAlbumURL: (id) => `https://api.spotify.com/v1/albums/${id}`,
+    ALBUMS_GET_ALBUMS_URL: 'https://api.spotify.com/v1/albums/',
     getAlbumsURL: () => module.exports.ALBUMS_GET_ALBUMS_URL,
-    ALBUMS_GET_ALBUM_TRACKS_URL: '/v1/albums/{id}/tracks',
-    getAlbumTracksURL: (id) => `/v1/albums/${id}/tracks`,
+    ALBUMS_GET_ALBUM_TRACKS_URL: 'https://api.spotify.com/v1/albums/{id}/tracks',
+    getAlbumTracksURL: (id) => `https://api.spotify.com/v1/albums/${id}/tracks`,
 
 
     //artists
-    ARTISTS_GET_ARTIST_URL: '/v1/artists/{id}',
-    getArtistsURL: (id) => `/v1/artists/${id}`,
-    ARTISTS_GET_ARTIST_ALBUMS_URL: '/v1/artists/{id}/albums',
-    getArtistAlbumbsURL: (id) => `/v1/artists/${id}/albums`,
-    ARTISTS_GET_ARTIST_TOPTRACKS_URL: '/v1/artists/{id}/top-tracks',
-    getArtistsTopTracksURL: (id) => `/v1/artists/${id}/top-tracks`,
-    ARTISTS_GET_ARTIST_RELATED_ARTISTS_URL: '/v1/artists/{id}/related-artists',
-    getArtistsRelatedArtistsURL: (id) => `/v1/artists/${id}/related-artists`,
-    ARTISTS_GET_ARTISTS_URL: '/v1/artists',
-    getArtistURL: (id) => `/v1/artists/${id}`,
+    ARTISTS_GET_ARTIST_URL: 'https://api.spotify.com/v1/artists/{id}',
+    getArtistsURL: (id) => `https://api.spotify.com/v1/artists/${id}`,
+    ARTISTS_GET_ARTIST_ALBUMS_URL: 'https://api.spotify.com/v1/artists/{id}/albums',
+    getArtistAlbumbsURL: (id) => `https://api.spotify.com/v1/artists/${id}/albums`,
+    ARTISTS_GET_ARTIST_TOPTRACKS_URL: 'https://api.spotify.com/v1/artists/{id}/top-tracks',
+    getArtistsTopTracksURL: (id) => `https://api.spotify.com/v1/artists/${id}/top-tracks`,
+    ARTISTS_GET_ARTIST_RELATED_ARTISTS_URL: 'https://api.spotify.com/v1/artists/{id}/related-artists',
+    getArtistsRelatedArtistsURL: (id) => `https://api.spotify.com/v1/artists/${id}/related-artists`,
+    ARTISTS_GET_ARTISTS_URL: 'https://api.spotify.com/v1/artists',
+    getArtistURL: () => modules.exports.ARTISTS_GET_ARTISTS_URL,
+
 
     //playlists
     /*
