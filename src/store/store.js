689b498f857612f5084021f44e08b10993d47788
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-02 21:12:08 -0700

    connect to redux extention

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,6 @@
 const {createStore} = require ('redux');
 const reducers = require ('./reducers/index.js')
 
-
-const store = createStore(reducers)
-
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 module.exports = store;
