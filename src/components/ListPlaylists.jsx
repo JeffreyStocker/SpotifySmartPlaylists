e4e618de358e929f3f9b4585c4b48daad23eaff1
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-04 12:56:39 -0700

    added basics to ListPlaylist

---
+++ src/components/ListPlaylists.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Grid,} from 'semantic-ui-react';
+
+export default function Playlists (props) {
+  return (
+    <div>
+      <Grid container columns={1}>
+        <Grid.Row>
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
+}
