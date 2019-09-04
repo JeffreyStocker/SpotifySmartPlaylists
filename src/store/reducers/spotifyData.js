e1281f2718ab3b24db6759edc99da71c8dcfb640
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-04 12:57:33 -0700

    init spotifyData reducer

---
+++ src/store/reducers/spotifyData.js
@@ -0,0 +1,12 @@
+const defaultData = {
+  artists: [],
+  playlists: [],
+  albums: [],
+  tracks: [],
+}
+
+const reducer = function (state = defaultData, action) {
+  return state
+}
+
+export default reducer;
