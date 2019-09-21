1f4e684eec3926b96bd6f7b0aa1a10381e35d916
Jeffrey Stocker <kagesennin@gmail.com>
2019-09-20 22:19:16 -0700

    refac app to properly show loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { HashRouter } from 'react-router-dom'
-
-import Menu from './components/Menu.jsx';
-import SmartPlaylists from './components/ListSmartPlaylists.jsx';
-import {Container, Loader} from 'semantic-ui-react';
 import cookie from'js-cookie';
 import axios from 'axios';
-import ShowSync from './components/ShowSync.jsx';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Menu from './components/Menu.jsx';
+import SmartPlaylists from './components/ListSmartPlaylists.jsx';
+import { Container, Loader, Message, Segment, Dimmer } from 'semantic-ui-react';
 import ListPlaylists from './components/ListPlaylists.jsx';
+import ShowSync from './components/ShowSync.jsx';
+
+import { HashRouter } from 'react-router-dom'
 
 import {connect} from 'react-redux';
 import {setUser} from './store/actions/user';
 import {setAllPlaylists} from './store/actions/smartPlaylists';
 
+const route = function (isLoaded, user) {
+  if (isLoaded) {
+    if (user.name === null) {
+      return <Message>Login To Continue</Message>
+    } else {
+      return <Switch>
+              <Route path="/smartplaylist" component={SmartPlaylists}></Route>
+              <Route path="/sync" component={ShowSync}></Route>
+              <Route path="/playlist" component={ListPlaylists}></Route>
+            </Switch>
+    }
+  } {
+    return <Segment style={{height: '100%'}}>
+            <Dimmer active>
+              <Loader>Loading User Data</Loader>
+            </Dimmer>
+          </Segment>
+  }
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoaded: false
+    };
   }
 
   componentDidMount() {
@@ -28,6 +51,15 @@ class App extends React.Component {
           this.props.setUser({name, id, accessToken, accessTokenExpire});
           this.props.setAllPlaylists(smartPlaylists);
         })
+        .catch(err => {
+          console.err (err);
+          this.props.setUser({
+            name: null
+          });
+        })
+        .then(() => {
+          this.setState({isLoaded: true})
+        })
     }
   }
 
@@ -36,14 +68,7 @@ class App extends React.Component {
       <HashRouter>
         <Container>
           <Menu></Menu>
-          {!this.props.user && <Message>Login To Continue</Message>}
-          {this.props.user &&
-            <Switch>
-              <Route path="/smartplaylist" component={SmartPlaylists}></Route>
-              <Route path="/sync" component={ShowSync}></Route>
-              <Route path="/playlist" component={ListPlaylists}></Route>
-            </Switch>
-          }
+          {route(this.state.isLoaded, this.props.user)}
         </Container>
       </HashRouter>
     );
