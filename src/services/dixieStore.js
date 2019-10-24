cca4127b1df8355b4fbfca42f31c9984a404058d
Jeffrey Stocker <kagesennin@gmail.com>
2019-10-23 19:59:45 -0700

    updated dixie stores

--- src/services/dixieStore.js
+++ src/services/dixieStore.js
@@ -3,10 +3,18 @@ import dexie from 'dexie';
 const db = new dexie('spotify')
 
 db.version(1).stores({
-  artists: '&id, genre',
-  tracks: '&id, *artists.id',
-  albums: '&id, *tracks.id',
-  playlists: '&id',
+  artists: '&id, name, *genre, popularity, followers.total',
+  tracks: '&id, name, *artists.id, popularity',
+  albums: '&id, name, label, album_type, *tracks.id, popularity, total_tracks',
+  playlists: '&id, name, followers.total',
 });
 
 export default db;
+
+export function filterArtists (callback) {
+  return db.artists.filter((artist) => {
+    return callback(artist)
+  }).toArray();
+}
+
+window.db = db;
