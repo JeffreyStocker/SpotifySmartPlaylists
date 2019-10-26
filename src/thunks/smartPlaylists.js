ae32b57277642d8240608c6f41380bcad9d0d326
Jeffrey Stocker <kagesennin@gmail.com>
2019-10-26 09:59:22 -0700

    added saveplaylist and removed send playlist when updating

--- src/thunks/smartPlaylists.js
+++ src/thunks/smartPlaylists.js
@@ -4,7 +4,7 @@ import store from '../store/store';
 import {updatePlaylist as updatePlaylistInStore} from '../store/actions/smartPlaylists';
 
 export async function updatePlaylist (userID, playlist) {
-  const results = await requestUpdatePlaylist(userID, playlist._id, playlist);
+  // const results = await requestUpdatePlaylist(userID, playlist._id, playlist);
   store.dispatch(updatePlaylistInStore(playlist));
 }
 
@@ -20,4 +20,13 @@ export async function updatePlaylistRule (playlist, indexOfRule, newRule) {
       console.error('could not save', err);
     }
   }
+}
+
+export async function savePlaylist (playlist) {
+  const {user} = store.getState();
+  const userID = user.name;
+  await requestUpdatePlaylist(userID, playlist._id, playlist);
+  playlist.modified = false;
+  console.log (playlist)
+  store.dispatch(updatePlaylistInStore(playlist));
 }
