0b78c87105a0055ba3c4c2ed49f232397e578f45
Jeffrey Stocker <kagesennin@gmail.com>
2019-10-26 14:12:27 -0700

    updated style

--- src/services/smartPlaylistServices.js
+++ src/services/smartPlaylistServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let updatePlaylistRequest = function (userID, playlistID) {
+const updatePlaylistRequest = function updatePlaylistRequest (userID, playlistID) {
   return axios.create({
     method: 'PATCH',
     baseURL: `/user/${userID}/smartplaylist/${playlistID}`,
@@ -8,33 +8,39 @@ let updatePlaylistRequest = function (userID, playlistID) {
       'Content-Type': 'application/json'
     },
   });
-}
+};
 
-export function deleteSmartPlaylist (userID, playlistID) {
+const deleteSmartPlaylist = function deleteSmartPlaylist (userID, playlistID) {
   return axios.delete (`/user/${userID}/smartplaylist/${playlistID}`)
     .then(results => {
       if (results.status !== 200) {
         throw new Errow (results);
       }
-    })
-}
+    });
+};
 
-export function requestAddToPlaylist  (userID) {
+const requestAddToPlaylist = function requestAddToPlaylist (userID) {
   return axios.post(`/user/${userID}/smartplaylist`)
     .then(response => response.data)
     .catch(err => {
       if (err.status === 404) {
-        throw new Error ()
+        throw new Error ();
       }
-    })
-}
+    });
+};
 
-export function updatePlaylist (userID, playlistID, playlistData) {
-  // const request = updatePlaylistRequest(userID, playlistID);
-
-  return axios.patch(`/user/${userID}/smartplaylist/${playlistID}`,{playlist: playlistData} ,  {
-    // data: JSON.stringify({playlist: playlistData}),
+const updatePlaylist = function updatePlaylist (userID, playlistID, playlistData) {
+  if (!userID || !playlistID || !playlistData) {
+    throw new Error ('Must supply all variables');
+  }
+  return axios.patch(`/user/${userID}/smartplaylist/${playlistID}`, {playlist: playlistData}, {
     headers: { 'Content-Type': 'application/json'}
-    }
-  )
-}
+  });
+};
+
+export {
+  updatePlaylist,
+  deleteSmartPlaylist,
+  requestAddToPlaylist,
+  updatePlaylistRequest
+};
